f9868f7019340ec8fe05e2dcca3002a76b798924
aiocrew <bodyanski1710@gmail.com>
2023-05-01 20:03:04 +0300

    положительные тесты на weathercontroller

--- WebApplication1.Tests/WeatherControllerTests.cs
+++ WebApplication1.Tests/WeatherControllerTests.cs
@@ -35,45 +35,48 @@ namespace WebApplication1.Tests
         [Fact]
         public async Task CreateTemperatureRecord_Returns_BadRequest_For_Null()
         {
-          
             var result = await _controller.CreateTemperatureRecord(null);
-
-            
             Assert.IsType<BadRequestObjectResult>(result);
+        }
+
+        [Fact]
+        public async Task CreateTemperatureRecord_Returns_Created_For_Valid_TemperatureRecord()
+        {
+            var temperatureRecord = new TemperatureRecord
+            {
+                Id = 1,
+                City = "Moscow",
+                Temperature = 20
+            };
+
+            _mockDbContext.Setup(db => db.TemperatureRecords.Add(temperatureRecord));
+
+            var result = await _controller.CreateTemperatureRecord(temperatureRecord);
+
+            Assert.IsType<StatusCodeResult>(result);
+            Assert.Equal(StatusCodes.Status201Created, (result as StatusCodeResult).StatusCode);
         }
 
         [Fact]
         public async Task UpdateTemperatureRecord_Returns_BadRequest_For_Null()
         {
-          
             var result = await _controller.UpdateTemperatureRecord(1, null);
-
-            
             Assert.IsType<BadRequestObjectResult>(result);
         }
 
         [Fact]
         public async Task DeleteTemperatureRecord_Returns_NotFound_For_Invalid_Id()
         {
-            
             _mockDbContext.Setup(db => db.TemperatureRecords.FindAsync(1)).ReturnsAsync((TemperatureRecord)null);
-
-            
             var result = await _controller.DeleteTemperatureRecord(1);
-
-            
             Assert.IsType<NotFoundObjectResult>(result);
         }
 
         [Fact]
         public async Task GetWeatherByCity_Returns_BadRequest_For_Empty_City()
         {
-            
             var result = await _controller.GetWeatherByCity(string.Empty);
-
-           
             Assert.IsType<BadRequestObjectResult>(result);
         }
-
     }
 }
